1293aaf2d0318232781208a129f2537c741cefa9
Dustin T Hill <dthill@syr.edu>
2024-05-30 13:55:07 -0400

    initial commit

---
+++ episewer-install-packages.R
@@ -0,0 +1,5 @@
+# install packages from CRAN
+install.packages("data.table", repos='http://cran.us.r-project.org')
+install.packages("ggplot2", repos='http://cran.us.r-project.org')
+install.packages("Matrix", repos='http://cran.us.r-project.org')
+install.packages("dplyr", repos='http://cran.us.r-project.org')
